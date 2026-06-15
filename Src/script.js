033176cfc62d83924e19f23b47dbfd69e6414cd6
nub1programer <jonfervicente@gmail.com>
2026-06-14 22:09:59 -0400

    Added something to the interface.

--- Src/script.js
+++ Src/script.js
@@ -1,7 +1,16 @@
 const playerOneScore = 0;
 const playerTwoScore = 0;
+const GoalTime = 0;
 
-const getRandomGoal = () => {
-    return Math.floor(Math.random() * (15 - 5 + 1)) + 5;
+const ChangeGoalBtn = document.getElementById("ChangeGoalBtn");
+const getRandomGoal = (goal) => {
+    return  goal = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
 }
 
+addEventListener("click", () => {
+    console.log(getRandomGoal(GoalTime));
+});
+
+
+console.log(getRandomGoal(GoalTime));
+    
